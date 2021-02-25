0e5cfa7d0a4ac35396740fbc325931b8d150f050
Li Zi Ying <ziying338@gmail.com>
2021-02-25 11:38:04 +0800

    modify gitignore and Sort.java

--- src/qp/operators/Sort.java
+++ src/qp/operators/Sort.java
@@ -1,14 +1,62 @@
-
+/** 
+ * Performs external sort on the file
+ */
 package qp.operators;
 
+import qp.utils.Batch;
+import qp.utils.Schema;
+import qp.utils.Tuple;
+
 public class Sort extends Operator {
-    public Sort(Operator operator) {
+    Operator base;      // base table to sort
+    Schema schema;      // base table schema
+    int batchsize;      // number of tuples per batch
+    int numBuffer;      // number of buffer available
+
+    public Sort(Operator operator, int numBuffer) {
         super(OpType.SORT);
+        this.base = base;
+        this.schema = base.schema;
+        this.numBuffer = numBuffer;
     }
 
-    public void CreateSortedRuns() {
+    /**
+     * opens connection to the base operator
+     */
+    @Override
+    public boolean open() {
+        if (!base.open()) {
+            return false;
+        }
+        System.out.println("HELLOHELLOHELLOHELLOHELLOHELLO");
+        // number of tuples per batch
+        int tupleSize = schema.getTupleSize();
+        batchsize = Batch.getPageSize() / tupleSize;
+
+        // generate sorted runs based on number of buffers
+        CreateSortedRuns(numBuffer);
+
+        return true;
+    }
+
+    /**
+     * Sorts next tuple from operator
+     */
+
+    public Batch next() {
+        return new Batch(batchsize);
+    }
+
+    /**
+     * Close the operator
+     */
+    public boolean close() {
+        return true;
+    }
+
+    public void CreateSortedRuns(int numBuffer) {
         // with B buffer pages, read in B pages each time
-        // sort recrods and produce a B page sorted run
+        // sort records and produce a B page sorted run
         // num_sorted_runs is ceiling (N/B)
     }
     
