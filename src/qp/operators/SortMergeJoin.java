5b126e0bf80e1f0cc450c0821fd4a5427bd28794
Li Zi Ying <ziying338@gmail.com>
2021-03-13 12:05:06 +0800

    set up smj

--- src/qp/operators/SortMergeJoin.java
+++ src/qp/operators/SortMergeJoin.java
@@ -1,9 +1,15 @@
 package qp.operators;
 
+import java.util.ArrayList;
+
+import qp.utils.Attribute;
 import qp.utils.Batch;
+import qp.utils.Condition;
 
 public class SortMergeJoin extends Join {
-    int batchsize;                  // Number of tuples per out batch
+    int batchSize;                  // Number of tuples per out batch
+    ArrayList<Integer> leftIndices;   // Indices of the join attributes in left table
+    ArrayList<Integer> rightIndices;  // Indices of the join attributes in right table
 
     public SortMergeJoin(Join jn) {
         super(jn.getLeft(), jn.getRight(), jn.getConditionList(), jn.getOpType());
@@ -13,13 +19,25 @@ public class SortMergeJoin extends Join {
     }
 
     public boolean open() {
+        int tuplesize = schema.getTupleSize();
+        batchSize = Batch.getPageSize() / tuplesize;
+
+        /** find indices attributes of join conditions **/
+        leftIndices = new ArrayList<>();
+        rightIndices = new ArrayList<>();
+        for (Condition con : conditionList) {
+            Attribute leftattr = con.getLhs();
+            Attribute rightattr = (Attribute) con.getRhs();
+            leftIndices.add(left.getSchema().indexOf(leftattr));
+            rightIndices.add(right.getSchema().indexOf(rightattr));
+        }
+
         return true;
     }
 
     public Batch next() {
-        int tuplesize = schema.getTupleSize();
-        batchsize = Batch.getPageSize() / tuplesize;
-        return new Batch(batchsize);
+        return new Batch(batchSize);
+       
     }
 
     public boolean close() {
