db7c9f62442007723be2c51a53eed02f3afc47f9
liawsy <liawsy99@gmail.com>
2021-03-13 18:53:08 +0800

    Create experiment tables and write queries

---
+++ testcases/experiment/run_experiment.bat
@@ -0,0 +1,14 @@
+java RandomDB FLIGHTS 20000
+java RandomDB AIRCRAFTS 20000
+java RandomDB SCHEDULE 20000
+java RandomDB CERTIFIED 20000
+java RandomDB EMPLOYEES 20000
+java ConvertTxtToTbl FLIGHTS
+java ConvertTxtToTbl AIRCRAFTS
+java ConvertTxtToTbl SCHEDULE
+java ConvertTxtToTbl CERTIFIED
+java ConvertTxtToTbl EMPLOYEES
+move *md*.* ..\..\classes
+move *stat*.* ..\..\classes
+move *tbl*.* ..\..\classes
+move *txt*.* ..\..\classes
---
+++ testcases/experiment/run_experiment.sh
@@ -0,0 +1,11 @@
+java RandomDB FLIGHTS 20000
+java RandomDB AIRCRAFTS 20000
+java RandomDB SCHEDULE 20000
+java RandomDB CERTIFIED 20000
+java RandomDB EMPLOYEES 20000
+java ConvertTxtToTbl FLIGHTS
+java ConvertTxtToTbl AIRCRAFTS
+java ConvertTxtToTbl SCHEDULE
+java ConvertTxtToTbl CERTIFIED
+java ConvertTxtToTbl EMPLOYEES
+mv *.md *.stat *.tbl *.txt ../../classes
