b2ef13df3eac1437d5e4f167b41f6f54d14815dd
Li Zi Ying <ziying338@gmail.com>
2021-03-14 19:34:06 +0800

    add smj logic

--- src/qp/operators/SortMergeJoin.java
+++ src/qp/operators/SortMergeJoin.java
@@ -8,20 +8,20 @@ import qp.utils.Condition;
 import qp.utils.Tuple;
 
 public class SortMergeJoin extends Join {
-    int batchSize;                  // Number of tuples per out batch
+    int batchSize;                    // Number of tuples per out batch
     ArrayList<Integer> leftIndices;   // Indices of the join attributes in left table
     ArrayList<Integer> rightIndices;  // Indices of the join attributes in right table
-    Batch outBatch;                 // Buffer page for output
-    Batch leftBatch;                // Buffer page for left input stream
-    Batch rightBatch;               // Buffer page for right input stream
+    Batch outBatch;                   // Buffer page for output
+    Batch leftBatch;                  // Buffer page for left input stream
+    Batch rightBatch;                 // Buffer page for right input stream
 
     int leftPtr;                      // Index pointer for left side buffer
-    int rightPtr;                      // Index pointer for right side buffer
-    boolean eosl;                   // Whether end of stream (left table) is reached
-    boolean eosr;                   // Whether end of stream (right table) is reached
-    Tuple leftTuple;                // current tuple from left table
-    Tuple rightTuple;                // current tuple from right table
-    ArrayList<Tuple> rightPartition; // collection of duplicates on right table
+    int rightPtr;                     // Index pointer for right side buffer
+    boolean eosl;                     // Whether end of stream (left table) is reached
+    boolean eosr;                     // Whether end of stream (right table) is reached
+    Tuple leftTuple;                  // current tuple from left table
+    Tuple rightTuple;                 // current tuple from right table
+    ArrayList<Tuple> rightPartition;  // collection of duplicates on right table
     int rightPartitionPtr;
 
     public SortMergeJoin(Join jn) {
@@ -57,6 +57,8 @@ public class SortMergeJoin extends Join {
         eosr = false;
         leftTuple = null;
         rightTuple = null;
+        leftBatch = null;
+        rightBatch = null;
         rightPartition = new ArrayList<Tuple>();
         rightPartitionPtr = 0;
 
@@ -71,29 +73,95 @@ public class SortMergeJoin extends Join {
         }
         
         // 1. if no batch read yet, read one batch in from both sides
+        if (leftBatch == null) {
+            leftBatch = left.next();
+        }
+        if (rightBatch == null) {
+            rightBatch = left.next();
+        }
 
-        
         outBatch = new Batch(batchSize);
-        // while outBatch is not full:
 
-        // 2. if right partition is empty:
-        
-        // compare left tuple and right tuple
-        // while left < right: advance left
-        // while left > right: advance right
-        // once equal: put right tuple into right partition
+        while (true) {
+            // if left batch is empty
+            if (leftBatch == null) {
+                eosl = true;
+                return null;
+            }
+            // if right batch is empty
+            if (rightBatch == null) {
+                eosr = true;
+                return null;
+            }
+            // if output batch is full, return it
+            if (outBatch.isFull()) {
+                return outBatch;
+            }
 
-        // 3. if left tuple == right tuple
-        // put join result into outBatch
-        // advance right ptr and save into right partition
+            leftTuple = leftBatch.get(leftPtr);
+            rightTuple = rightBatch.get(rightPtr);
 
-        // 4. else left tuple != right tuple
-        // means right tuple has gone too far
-        // advance left ptr and loop to the start of right partition
+            if (rightPartition.isEmpty()) {
+                // while left < right: advance left
+                while (Tuple.compareTuples(leftTuple, rightTuple, leftIndices, rightIndices) < 0) {
+                    leftTuple = getNextLeftTuple();
+                }
 
-        return outBatch;
-       
+                // while left > right: advance right
+                while (Tuple.compareTuples(leftTuple, rightTuple, leftIndices, rightIndices) > 0) {
+                    rightTuple = getNextRightTuple();
+                }
+
+                // once equal: put right tuple into right partition
+                rightPartition.add(rightTuple);
+                rightPartitionPtr = 0;
+            }
+
+            
+            if (leftTuple.checkJoin(rightTuple, leftIndices, rightIndices)) {
+                // put join result into outBatch
+                Tuple joinResult = leftTuple.joinWith(rightTuple);
+                outBatch.add(joinResult);   
+                // advance right ptr and save into right partition            
+                rightTuple = getNextRightTuple();
+                rightPartition.add(rightTuple);
+                rightPartitionPtr++;
+            } else {
+                // means right tuple has gone too far
+                // advance left ptr and loop to the start of right partition     
+                leftTuple = getNextLeftTuple();
+                rightPartitionPtr = 0;
+            }
+        }
+
     }
+    
+    private Tuple getNextLeftTuple() {
+        if (leftPtr == batchSize - 1) {
+            leftBatch = left.next();
+            if (leftBatch == null) {
+                eosl = true;
+                return null;
+            }
+        }
+
+        leftPtr = (leftPtr + 1) % batchSize;
+        return leftBatch.get(leftPtr);
+    }
+
+    private Tuple getNextRightTuple() {
+        if (rightPtr == batchSize - 1) {
+            rightBatch = right.next();
+            if (rightBatch == null) {
+                eosr = true;
+                return null;
+            }
+        }
+
+        rightPtr = (rightPtr + 1) % batchSize;
+        return rightBatch.get(rightPtr);
+    }
+    
 
     public boolean close() {
         left.close();
