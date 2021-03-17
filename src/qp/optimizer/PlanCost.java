46efc841c4b835f3a075fd077a50470bca9caf2a
Li Zi Ying <ziying338@gmail.com>
2021-03-17 16:08:40 +0800

    add cost for smj

--- src/qp/optimizer/PlanCost.java
+++ src/qp/optimizer/PlanCost.java
@@ -230,8 +230,13 @@ public class PlanCost {
                 joincost = leftpages * rightpages;
                 break;
             case JoinType.SORTMERGE:
-                // change later
-                joincost = leftpages * rightpages;
+                // cost to sort left
+                long sortLeftCost = externalSortCost(leftpages, numbuff);
+                // cost to sort right
+                long sortRightCost = externalSortCost(rightpages, numbuff);
+                // merge cost
+                long mergeCost = leftpages + rightpages;
+                joincost = sortLeftCost + sortRightCost + mergeCost;
                 break;
             default:
                 System.out.println("join type is not supported");
@@ -240,6 +245,11 @@ public class PlanCost {
         cost = cost + joincost;
 
         return outtuples;
+    }
+
+    private long externalSortCost(long numPages, long numBuff) {
+        int numPass = 1 + (int) Math.ceil(Math.log(Math.ceil(numPages / (1.0 * numBuff))) / Math.log(numPages - 1) );
+        return numPass * (2 * numPages);
     }
 
     /**
